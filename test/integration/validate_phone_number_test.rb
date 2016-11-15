5794cc27972e2d29437262b7aed1d1d25bdb2b0a
dusan <dushan@rubydonkey.com>
2016-11-16 00:29:06 +0100

    Just a small style changes

--- test/integration/validate_phone_number_test.rb
+++ test/integration/validate_phone_number_test.rb
@@ -1,39 +1,40 @@
 require 'test_helper'
 
 class ValidatePhoneNumberTest < ActionDispatch::IntegrationTest
+  INVALID_PHONE_NUMBER = "+3816453"
 
-test "Invalid phone number" do
+  test "Invalid phone number" do
     get root_path
     assert_response :success
-    assert_equal '/', path
-    post "/signin/show", params: {:phone_number => "+3816453"}
+    assert_equal root_path, path
+    post "/signin/show", params: {:phone_number => INVALID_PHONE_NUMBER}
     assert_equal '/signin/show', path
-    assert_select 'p', "You have entered wrong phone number format, please try again!"
-
+    assert_select 'p', I18n.t('phone_number.invalid')
+    assert_select 'input[value=?]', INVALID_PHONE_NUMBER
   end
 
-test "Phone number blank" do
+  test "Phone number blank" do
     get root_path
     assert_response :success
-    assert_equal '/', path
+    assert_equal root_path, path
     post "/signin/show", params: {:phone_number => ""}
     assert_equal '/signin/show', path
-    assert_select 'p', "Empty phone number!"
+    assert_select 'p', I18n.t('phone_number.blank')
 
   end
 
   ["+381652932222", "381652932222", "+381(65)293-2222", "+381 65 293 22 22"].each_with_index do |phone_number, i|
     test "Valid phone number #{i}" do
-        get root_path
-        assert_response :success
-        assert_equal '/', path
-        post "/signin/show", params: {"phone_number" => phone_number}
+      get root_path
+      assert_response :success
+      assert_equal root_path, path
+      post "/signin/show", params: {"phone_number" => phone_number}
 
-        follow_redirect!
+      follow_redirect!
 
-        assert_equal '/signin/secret_token', path
-        assert_select 'p', "This page serves secret token for phone number: +381652932222"
+      assert_equal '/signin/secret_token', path
+      assert_select 'input.disabled[value=?]', '+381652932222'
 
-      end
+    end
   end
 end
