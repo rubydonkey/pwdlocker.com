83e614734adf66a89f2fba8460f4a29c04a62477
dusan <dushan@rubydonkey.com>
2016-11-15 23:54:54 +0100

    Improved phone number tests a bit

--- test/integration/validate_phone_number_test.rb
+++ test/integration/validate_phone_number_test.rb
@@ -1,12 +1,12 @@
 require 'test_helper'
 
 class ValidatePhoneNumberTest < ActionDispatch::IntegrationTest
-  
+
 test "Invalid phone number" do
     get root_path
     assert_response :success
     assert_equal '/', path
-    post "/signin/show", :phone_number => "+3816453" 
+    post "/signin/show", params: {:phone_number => "+3816453"}
     assert_equal '/signin/show', path
     assert_select 'p', "You have entered wrong phone number format, please try again!"
 
@@ -16,20 +16,24 @@ test "Phone number blank" do
     get root_path
     assert_response :success
     assert_equal '/', path
-    post "/signin/show", :phone_number => "" 
+    post "/signin/show", params: {:phone_number => ""}
     assert_equal '/signin/show', path
     assert_select 'p', "Empty phone number!"
 
   end
 
-test "Valid phone number" do
-    get root_path
-    assert_response :success
-    assert_equal '/', path
-    post_via_redirect "/signin/secret_token", {"phone"=>'+381652932222', "national_format"=>"065 2932222", "country_code" => "RS"}
-    assert_equal '/signin/secret_token', path
-    assert_select 'p', "This page serves secret token for phone number: +381652932222"
+  ["+381652932222", "381652932222", "+381(65)293-2222", "+381 65 293 22 22"].each_with_index do |phone_number, i|
+    test "Valid phone number #{i}" do
+        get root_path
+        assert_response :success
+        assert_equal '/', path
+        post "/signin/show", params: {"phone_number" => phone_number}
 
+        follow_redirect!
+
+        assert_equal '/signin/secret_token', path
+        assert_select 'p', "This page serves secret token for phone number: +381652932222"
+
+      end
   end
-
 end
