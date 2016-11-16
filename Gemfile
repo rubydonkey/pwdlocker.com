3bc99fbe2fb34f86156db01f639d304a124963cc
Dushan <netoff@users.noreply.github.com>
2016-11-16 13:41:26 +0100

    We need page for user to sign in (#1)
    * We need page for use to sign in
    
    * Fixed app.json file
    
    * signin input form
    
    * Signin change
    
    * signin modification
    
    * Signin modification
    
    * modified signin_controller
    
    * modifications
    
    * Empty phone number-fixed (by JavaScript)
    
    * refactored signin/secret token, with integratino tests
    
    * Secret token view improvement
    
    * signin with tests
    
    * Improved phone number tests a bit
    
    * Just a small style changes
    
    * Use path names in tests instead of hard codded paths

--- Gemfile
+++ Gemfile
@@ -46,3 +46,9 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'twilio-ruby', '4.13.0'
+
+gem 'rails-controller-testing'
+
+gem 'capybara'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "pages#index"
+  root to: "signin#new"
+
+  get '/signin', to: 'signin#new', as: :signin
+
+  get  'signin/show'
+  post 'signin/show'
+  get  'signin/secret_token'
+  post 'signin/secret_token'
+
+
 end
