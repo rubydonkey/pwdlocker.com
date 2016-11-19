b050c50e7e7b51ce44f32b50ae13ae5a671b843b
Zarko Milosevic <zarko@kg.ac.rs>
2016-11-19 15:51:00 +0100

    Version without blocks

--- app/helpers/passwords_helper.rb
+++ app/helpers/passwords_helper.rb
@@ -1,2 +1,12 @@
+require 'uri'
 module PasswordsHelper
+
+  def get_favicon_url(password)
+    url = password.URL
+    if(!URI.parse(url).scheme)
+      url = "https://#{url}"
+    end
+    "#{URI.parse(url).to_s}/favicon.ico"
+  end
+
 end
