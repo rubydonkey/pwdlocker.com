e30865ec183510fb68f96432b80d902c76b4e8ec
Zarko Milosevic <zarko@kg.ac.rs>
2016-11-24 20:52:00 +0100

    Old migration reparied

--- db/migrate/20161103192709_create_passwords.rb
+++ db/migrate/20161103192709_create_passwords.rb
@@ -4,7 +4,7 @@ class CreatePasswords < ActiveRecord::Migration[5.0]
       t.string :title
       t.string :URL
       t.string :username
-      t.text :password
+      t.string :password
 
       t.timestamps
     end
