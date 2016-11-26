4a55c1ad1d7984746bf455ee59fa5dc791b66def
Zarko Milosevic <zarko@kg.ac.rs>
2016-11-26 19:20:50 +0100

    Added before save check for schema in url

--- app/models/password.rb
+++ app/models/password.rb
@@ -1,5 +1,8 @@
-class Password < ApplicationRecordp
-  require 'uri'
+require 'uri'
+
+class Password < ApplicationRecord
+  before_save :check_url
+  before_update :check_url
 
   validates :title, presence: true
   validates :username, presence: true
@@ -9,4 +12,14 @@ class Password < ApplicationRecordp
 
   VALID_URL_REGEX = /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
   # validates :URL, presence: true, format: { with: URI::DEFAULT_PARSER.regexp[:ABS_URI] }
+
+  private
+
+  def check_url
+    uri = URI.parse(self.URL)
+    if(!uri.scheme)
+      self.URL = "http://#{self.URL}"
+    end
+  end
+
 end
