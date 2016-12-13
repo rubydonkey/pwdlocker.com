4b22e604025c1b39449bb6779f0e67f9c006ea12
banecikic <banebenec@gmail.com>
2016-12-14 00:48:25 +0100

    add group to password, then save password

--- db/migrate/20161205222153_create_password_groups.rb
+++ db/migrate/20161205222153_create_password_groups.rb
@@ -1,7 +1,7 @@
 class CreatePasswordGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :password_groups do |t|
-      t.string :name
+      t.string :name, default: "empty"
 
       t.timestamps
     end
