02c58277015d3265cee9cd47dc0a72a0464a8725
Dushan <netoff@users.noreply.github.com>
2017-01-15 17:30:51 +0100

    Update wait_for_ajax.rb
    Added TODO comment for later refactor

--- test/support/wait_for_ajax.rb
+++ test/support/wait_for_ajax.rb
@@ -1,3 +1,7 @@
+#TODO refactor tests itself, so `wait_for_ajax` is not needed, 
+# and never rely on custom helpers in such a case. When capybara 
+# finders are used properly to select elements, Capybara should be
+# able to handle all Ajax requests properly
 module WaitForAjax
   def wait_for_ajax
     Timeout.timeout(Capybara.default_max_wait_time) do
