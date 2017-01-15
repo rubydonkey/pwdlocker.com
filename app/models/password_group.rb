5fb37356cfd95d88d737aead0c9a5614a157a1e3
banecikic <banebenec@gmail.com>
2017-01-15 22:33:24 +0100

    unique password group name

--- app/models/password_group.rb
+++ app/models/password_group.rb
@@ -1,5 +1,6 @@
 class PasswordGroup < ApplicationRecord
   has_many :passwords
   validates :name, presence: true
+  validates_uniqueness_of :name, :message => " has already been taken!"
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161215134022) do
+ActiveRecord::Schema.define(version: 20170112223043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20161215134022) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_password_groups_on_name", unique: true, using: :btree
   end
 
   create_table "passwords", force: :cascade do |t|
