80bba5016478f29969de9c4aa935de25c9415aed
zarkomilosevic <zarko@kg.ac.rs>
2017-02-13 22:32:08 +0100

    React password done. Some test errors need to be fixed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,21 @@ class PagesController < ApplicationController
 
   def index
     @passwords = Password.all
+    @password_groups = Array.new()
+    @favicon_URIs = Array.new()
+    @timestamps = Array.new()
+
+    @passwords.each do |pwd|
+      @password_groups.push(pwd.password_group.try(:name))
+      @favicon_URIs.push(pwd.favicon_DataURI)
+
+      if(pwd.password_last_changed_at.present?)
+        @timestamps.push(pwd.password_last_changed_at.strftime("%b %d, %Y %H:%M:%S GMT"))
+      else
+        @timestamps.push(pwd.created_at.strftime("%b %d, %Y %H:%M:%S GMT"))
+      end
+    end
+
   end
 
 end
