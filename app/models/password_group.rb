5aac0d796e55b02d4a14808b5c81745e76f9ed5e
Zarko Milosevic <zarko@kg.ac.rs>
2017-02-27 22:30:08 +0100

    overriden as_json

--- app/models/password_group.rb
+++ app/models/password_group.rb
@@ -2,4 +2,8 @@ class PasswordGroup < ApplicationRecord
   has_many :passwords
   validates :name, presence: true
   
+  def as_json(options = nil)
+    super({ only: [:name]}.merge(options || {}))
+  end
+  
 end
