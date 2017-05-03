1e8959188f51418166e4a73944736207651c2502
banecikic <banebenec@gmail.com>
2017-05-03 10:38:41 +0200

    fuse search

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,4 @@ Rails.application.routes.draw do
   get     'login'     =>  'sessions#new'
   post    'login'     =>  'sessions#create'
   delete  'logout'    =>  'sessions#destroy'
-
-  get     'fuseSearch' => 'pages#fuseSearch'
 end
