ff846c90ab250cb0fd906f6cd239c7a58f57cba9
Dusan <dushan@rubydonkey.com>
2017-09-27 23:55:15 +0200

    Remove duplicate of react included with a package

--- app/javascript/pwdlocker/views/SearchForm.js
+++ app/javascript/pwdlocker/views/SearchForm.js
@@ -4,17 +4,9 @@
 
 import React from 'react';
 
-import HotKey from 'react-shortcut';
-
 function SearchForm(props) {
-    const searchKeys = ['ctrl', 'f'];
-
     return (
         <form className="navbar-form navbar-left">
-          <HotKey
-            keys={searchKeys}
-            simultaneous
-            onKeysCoincide={(e)=>{ e.preventDefault(); console.log('search pressed<<--'); return false;}} />
           <input value={props.searchString}
                onChange={(e) => props.onChangeSearchString(e.target.value)}
                placeholder="Search passwords..."
